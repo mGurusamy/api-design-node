a6e9cb09a872859f2d4da1ede96e932172e138d6
Manoharan Gurusamy <mgurusamy@UKLMGURUSAMY.local>
2018-07-14 12:25:18 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -3,3 +3,25 @@
 // it should then send back jsonData on a GET to /data
 
 var jsonData = {count: 12, message: 'hey'};
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html'), function(err){
+        if(err){
+            res.status(500).send(err);
+        }
+    }
+});
+
+app.get('/data', function(req, res){
+    res.json(jsonData);
+});
+
+app.listen(port, function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log('App is up and Running in port number ' + port);
+    }
+});
