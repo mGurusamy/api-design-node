522d69b949bb82a3ee26223c8e5d2d37decca1b3
Manoharan Gurusamy <mgurusamy@UKLMGURUSAMY.local>
2018-07-18 22:29:40 +0100

    Changes applied for tiger Resources

--- server/tigers.js
+++ server/tigers.js
@@ -2,3 +2,26 @@
 // and make some REST routes for it, exactly like for lions
 // make a middleware that just logs the word 'tiger' to the console
 // when a request comes in to the server
+
+var tigerRouter = require('express').Router();
+
+var tigers =[];
+var id = 0;
+
+tigerRouter.get('/', function(req, res){
+    res.join(tigers);
+});
+
+tigerRouter.param('id', function(err, req, res, next){
+    var tiger = _.find(tigers, {id: id});
+    if(tiger){
+        req.tiger = tiger;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+tigerRouter.get('/:id', function(req, res){
+    res.join(req.tiger);
+});
+module.exports = tigerRouter;
